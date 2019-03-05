3f26230d7fbe3494bff81dd0e8c7687dec524d66
qwmam <48229643+qwmam@users.noreply.github.com>
2019-03-05 12:00:04 +0800

    Update and rename src/main/java/com/neusoft/App.java to  src/ main/ java/ com/ neusoft/App.java

--- src/ main/ java/ com/ neusoft/App.java
+++ src/ main/ java/ com/ neusoft/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Welcome to" );
+        System.out.println( "Hello World! Welcome to"  );
+        wwdgniaxhuc1ishilihs
     }
 }
